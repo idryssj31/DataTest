8c2db7c3a02b65e00ad7627635579f61260cb45f
idryssj31 <55395114+idryssj31@users.noreply.github.com>
2025-03-26 09:28:48 +0100

    [UPD] : data line ueless

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
 
 # URL de la page à scraper
 url = 'https://en.volleyballworld.com/volleyball/competitions/volleyball-nations-league/2022/finals-statistics/men/best-receivers/'
@@ -33,11 +34,27 @@ if response.status_code == 200:
 else:
     print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
-# Lire le fichier CSV
-df = pd.read_csv(r'c:\Users\idrys\DataTest\JO\JO2020\JOTROPHIES\jotrophies2020.csv')
+# Chemin du répertoire contenant les fichiers CSV
+directory = r'c:\Users\idrys\DataTest'
 
-# Supprimer les colonnes 'web-scraper-order' et 'web-scraper-start-url'
-df = df.drop(columns=['web-scraper-order', 'web-scraper-start-url'])
-
-# Sauvegarder le fichier CSV modifié
-df.to_csv(r'c:\Users\idrys\DataTest\JO\JO2020\JOTROPHIES\jotrophies2020_modified.csv', index=False)
+# Parcourir tous les fichiers dans le répertoire
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        if file.endswith('.csv'):
+            file_path = os.path.join(root, file)
+            try:
+                # Lire le fichier CSV
+                df = pd.read_csv(file_path)
+                
+                # Vérifier si les colonnes existent dans le fichier CSV
+                if 'web-scraper-order' in df.columns and 'web-scraper-start-url' in df.columns:
+                    # Supprimer les colonnes 'web-scraper-order' et 'web-scraper-start-url'
+                    df = df.drop(columns=['web-scraper-order', 'web-scraper-start-url'])
+                    
+                    # Sauvegarder le fichier CSV modifié
+                    df.to_csv(file_path, index=False)
+                    print(f"Updated file: {file_path}")
+                else:
+                    print(f"Columns not found in file: {file_path}")
+            except Exception as e:
+                print(f"Failed to process file: {file_path}. Error: {e}")
